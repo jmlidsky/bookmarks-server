c29d5ac81b68922550aad5a13206067987b0eb67
Jill Lidsky <jilllidsky15@gmail.com>
2020-04-14 16:07:08 -0400

    added files, endpoints for get, post, and delete

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    id: 1,
+    title: 'Reddit',
+    url: 'https://www.reddit.com/',
+    description: 'front page of the internet',
+    rating: 5
+}]
+
+module.exports = { bookmarks }
